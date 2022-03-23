8d50caada20b62d3b50ece6dfd8b3e5dce8ea4d4
NicolasLavaury <91453854+NicolasLavaury@users.noreply.github.com>
2022-03-23 23:40:49 +0100

    Add files via upload

---
+++ Netflix - Part6/assets/video.js
@@ -0,0 +1,7 @@
+document.querySelector(".videoplay").addEventListener("mouseover", function() {
+    this.play()
+     })
+
+     document.querySelector(".videoplay").addEventListener("mouseleave", function() {
+         this.pause()
+ })
